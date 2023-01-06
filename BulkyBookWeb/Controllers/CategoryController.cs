5d8f4690420e0e687681c9f8474962167e401d7d
marqueeze <marknovak85@gmail.com>
2023-01-06 19:11:38 +0100

    Added delete functionality / Temp data after post

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyBookWeb.Data;
 using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Controllers
 {
@@ -37,9 +38,71 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category uspesno created";
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        
+       
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var getCategoryFromDb = _db.Categories.Find(id);
+
+            if (getCategoryFromDb == null)
+            {
+                return NotFound();  
+            }
+
+            return View(getCategoryFromDb);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category uspesno apdejtovana";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var getCategoryFromDb = _db.Categories.Find(id);
+
+            if (getCategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(getCategoryFromDb);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            TempData["success"] = "Category uspesno obrisana";
+            return RedirectToAction("Index");
         }
     }
 }
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -8,7 +8,7 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        [Range(1,2, ErrorMessage="Ne valja ti ovaj range")]
+        [Range(1,22, ErrorMessage="Ne valja ti ovaj range")]
         public int DisplayOrder { get; set; }   
         public DateTime CreatedDate { get; set; } = DateTime.Now;
 
